f3f6a94c1a460b4d97d8e393829b658d92d8c1e0
kwpid <143108314+kwpid@users.noreply.github.com>
2025-04-23 17:55:49 -0400

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, doc, setDoc, getDoc, updateDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Collections
+const usersCollection = collection(db, "users");
+const cardsCollection = collection(db, "cards");
+const packsCollection = collection(db, "packs");
+
+export { db, usersCollection, cardsCollection, packsCollection, doc, setDoc, getDoc, updateDoc };
