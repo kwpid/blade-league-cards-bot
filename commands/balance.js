55b9181a1471943c7fd6e7393d845a479260743d
kwpid <143108314+kwpid@users.noreply.github.com>
2025-04-23 17:57:09 -0400

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -1,25 +1,5 @@
 import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
-import fs from 'fs/promises';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const dataPath = path.join(__dirname, '../data/userBalances.json');
-
-async function ensureDataFile() {
-  try {
-    await fs.access(dataPath);
-  } catch {
-    await fs.mkdir(path.dirname(dataPath), { recursive: true });
-    await fs.writeFile(dataPath, '{}');
-  }
-}
-
-async function getBalance(userId) {
-  await ensureDataFile();
-  const data = JSON.parse(await fs.readFile(dataPath, 'utf8'));
-  return data[userId] || 100;
-}
+import { getUserData } from "../firestoreHelpers.js";
 
 function formatStars(number) {
   if (number < 1000) return number.toString();
@@ -43,8 +23,8 @@ export default {
 
   async execute(interaction) {
     const targetUser = interaction.options.getUser("user") || interaction.user;
-    const balance = await getBalance(targetUser.id);
-    const formatted = formatStars(balance);
+    const userData = await getUserData(targetUser.id);
+    const formatted = formatStars(userData.balance);
 
     const embed = new EmbedBuilder()
       .setColor(0xffd700)
